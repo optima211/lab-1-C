ac50084efdeb526ae7b1df1af74c77c7dcdb9c42
optima211 <nedmk@mail.ru>
2018-11-29 23:09:28 +0700

    delete not used functions

--- main.c
+++ main.c
@@ -4,29 +4,21 @@
 
 # define n 5 // определение константы n=50
 
-int i, max, nom, a[n]; //описание массива целых чисел из n элементов
-int main()
+int i, a[n], b[n]; //описание массива целых чисел из n элементов
+int main() {
+    for (i = 0; i < n; i++) {
+        printf("\n enter element first massive ");
 
-{for (i=0; i<n; i++)
-
-    {
-    printf("\n enter element massive ");
-
-        scanf_s ("%d", &a[i]);
+        scanf_s("%d", &a[i]);
     }
+    for (i = 0; i < n; i++) {
+        printf("\n enter element second massive ");
 
-    for (i=1,max=a[0],nom=0; i<n; i++)
-
-        if (max<a[i])
-
-        {nom=i; max=a[i];}
-
-    printf("\n out start massive : \n");
-
-    for (i=0; i<n; i++) printf ( "%6d", a[i] );
-
-    printf ("\n max element: %4d, index: %4d " , max, nom);
-
+        scanf_s("%d", &b[i]);
+    }
+    printf("\n out first start massive : \n");
+    for (i = 0; i < n; i++) printf("%6d", a[i]);
+    printf("\n out second start massive : \n");
+    for (i = 0; i < n; i++) printf("%6d", b[i]);
     return 0;
-
 }
