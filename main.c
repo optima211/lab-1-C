433e51bc95237b17b807f40b32da9167a6aeadb8
optima211 <nedmk@mail.ru>
2018-11-30 22:33:15 +0700

    create function 'sort' without transfer in-out

--- main.c
+++ main.c
@@ -3,22 +3,29 @@
 #include <conio.h>
 
 # define n 5 // определение константы n=50
-
 int i, a[n], b[n]; //описание массива целых чисел из n элементов
+
+int sort() {
+    printf("\nhello!");
+    return 0;
+}
+
 int main() {
     for (i = 0; i < n; i++) {
         printf("\n enter element first massive ");
-
         scanf_s("%d", &a[i]);
     }
+
     for (i = 0; i < n; i++) {
         printf("\n enter element second massive ");
-
         scanf_s("%d", &b[i]);
     }
+
     printf("\n out first start massive : \n");
     for (i = 0; i < n; i++) printf("%6d", a[i]);
     printf("\n out second start massive : \n");
     for (i = 0; i < n; i++) printf("%6d", b[i]);
+
+    sort();
     return 0;
 }
